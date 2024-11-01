861bb2acb28ed367f07b0b71ae02be1103b467d0
Erfanmah7 <efn8mah@yahoo.com>
2024-11-01 20:50:13 +0330

    getStaticPaths SSG dynamic

--- pages/users/[userId].js
+++ pages/users/[userId].js
@@ -1,7 +1,42 @@
-import React from "react";
-
-function userId() {
-  return <div>userId</div>;
+function userId({ usersId }) {
+  return (
+    <div>
+      <h3>{usersId.name}</h3>
+      <p>{usersId.email}</p>
+    </div>
+  );
 }
 
 export default userId;
+
+//1 dynamic route
+export async function getStaticPaths() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await res.json();
+
+  const paths = users.map((user) => ({
+    params: {
+      userId: user.id.toString(),
+    },
+  }));
+
+  return {
+    // paths: [{ params: { userId: "1" } }],
+    paths,
+    fallback: false,
+  };
+}
+
+//2
+export async function getStaticProps(context) {
+  //userId
+  const { params } = context;
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${params.userId}`
+  );
+  const usersId = await res.json();
+
+  return {
+    props: { usersId },
+  };
+}
--- pages/users/index.js
+++ pages/users/index.js
@@ -1,7 +1,26 @@
-import React from "react";
+import Link from "next/link";
 
-function Users() {
-  return <div>Users</div>;
+function Users({ users }) {
+  return (
+    <div>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <Link href={`/users/${user.id}`}>{user.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Users;
+
+export async function getStaticProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await res.json();
+
+  return {
+    props: { users },
+  };
+}
